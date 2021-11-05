d7fd321bb64f0bbd5d4286a534597b438be501a8
Peter Pan <peter@never.lan>
2021-11-05 16:48:10 +0000

    shell commands working

--- repl.rb
+++ repl.rb
@@ -1,8 +1,11 @@
 require 'readline'
 
 while input = Readline.readline("=> ", true)
-    if input == "exit"
+    parsed1 = input.split
+    if parsed1[0] == "exit"
         break
+    elsif (`which #{parsed1[0]}`.length >= 1)
+        puts `#{input}`
     else
         puts "#{eval(input.to_s)}"
     end
